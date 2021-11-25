49bacb625d6dc6a6b2d6e5b6e24e8f74e06d7b65
iuliki <iulia.bulugu@emanuel.ro>
2021-11-25 16:17:01 +0200

    exercitiul1

--- vectori1/vectori1/Program.cs
+++ vectori1/vectori1/Program.cs
@@ -6,7 +6,88 @@ namespace vectori1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            PrintArray(array);
+            Console.WriteLine("\n");
+            suma(array);
+            Console.WriteLine("\n");
+            med(array);
+            Console.WriteLine("\n");
+            elementePare(array);
+            Console.WriteLine("\n");
+            MinMax(array);
+
+
         }
+        static void PrintArray(int[] array)
+        {
+            if (array is null)
+            {
+                Console.WriteLine("array is null");
+                return;
+            }
+            Console.Write("array= ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                int element = array[i];
+                Console.Write(element);
+                if (i < array.Length - 1)
+                {
+                    Console.Write(",");
+                }
+            }
+        }
+
+        static void suma(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum = sum + array[i];
+            }
+            Console.Write("suma cifrelor din vector este "+ sum);
+        }
+
+        static void med (int [] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum = sum + array[i];
+            }
+            int media = sum / array.Length;
+            Console.Write("media cifrelor din vector este " + media);
+        }
+
+        static void elementePare(int [] array)
+        {
+            int nr = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                    nr++;
+            }
+
+            Console.WriteLine("numarul elementelor pare din vector= " + nr);
+        }
+
+        static void MinMax(int[] array)
+        {
+            int min = array[0];
+            int max = array[0];
+
+            for(int i=1;i<array.Length;i++)
+            {
+                if (min > array[i])
+                    min = array[i];
+                else
+                    if (max < array[i])
+                    max = array[i];
+            }
+
+            Console.WriteLine("minimul este " + min +" iar maximul este " + max);
+
+        }
+        
     }
 }
